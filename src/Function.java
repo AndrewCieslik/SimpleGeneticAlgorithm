22d4c0f2655e9623c8c453fa39ce0ccd3a33c6e3
cieslik.andrew <cieslik.andrew@gmail.com>
2023-11-26 08:08:02 +0100

    a,b,c to main

--- src/Function.java
+++ src/Function.java
@@ -1,10 +1,12 @@
 public class Function {
-    static double fx, x;
-    static int a = 1;
-    static int b = -250;
-    static int c = 10000;
+    int c;
+    private int fx;
+    private int x;
+    private int a;
+    privte
+    private int b;
 
-    static double fxPositive(double x) {
+    static double fxPositive(int x) {
         fx = a * Math.pow(x, 2) + b * x + c;
         if (fx < 0) {
             fx = fx - fx + 1;   //+1 don't kill the weakest one
--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,16 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int populationSize = 18;
+        int a = 1;
+        int b = -250;
+        int c = 10000;
+        int populationSize = 4;
         int chromosomeSize = 8;
         int numOfIterations = 40;
         FileWriter writer = new FileWriter("results.txt");
 
         while (numOfIterations > 0) {
-            int numOfGenerations = 8;
+            int numOfGenerations = 1;
             Population population = new Population(populationSize, chromosomeSize);
             Population newGeneration = new Population(populationSize, chromosomeSize);
             while (numOfGenerations > 0) {
